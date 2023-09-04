2c15e26d9bc4029870f1e4882bdfdde500a0d7fc
Artur <kolosokbb@mail.ru>
2023-09-04 16:42:45 +0300

    fixes

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,4 +1,3 @@
-import axios, {AxiosInstance} from "axios";
 import {ITodo} from "../models/todo";
 import {IList} from "../models/list";
 import {
@@ -13,17 +12,27 @@ import {
 export type ApiMethodAgs<T extends (...args: any[]) => Promise<any>> = Awaited<Parameters<T>>
 
 class ApiServiceClazz {
-    private readonly instance: AxiosInstance = axios.create({
-        baseURL: 'http://mobile-dev.oblakogroup.ru/candidate/qa-mobile',
-        headers: {
-            Accept: 'application/json'
+    private url = 'http://mobile-dev.oblakogroup.ru/candidate/qa-mobile';
+
+    private instance = {
+        get: <T>(url: string) => {
+            return fetch(this.url + url, { method: 'GET',  headers: { Accept: 'application/json', 'Content-Type': 'application/json' } })
+        },
+        delete: <T>(url: string) => {
+            return fetch(this.url + url, { method: 'DELETE',  headers: { Accept: 'application/json', 'Content-Type': 'application/json' } })
+        },
+        post: <T>(url: string, data?: any) => {
+            return fetch(this.url + url, { method: 'POST', body: JSON.stringify(data), headers: { Accept: 'application/json', 'Content-Type': 'application/json' } })
+        },
+        patch: <T>(url: string, data: any) => {
+            return fetch(this.url + url, { method: 'PATCH', body: JSON.stringify(data),  headers: { Accept: 'application/json', 'Content-Type': 'application/json' } })
         }
-    })
+    }
 
     public async getList() {
         try {
-            const resp = await this.instance.get<IList[]>(`/list`)
-            return resp.data
+            const resp = await this.instance.get(`/list`)
+            return await resp.json() as Promise<IList[]>;
         } catch (e) {
             console.log('getList ERROR: ', e)
             throw e;
@@ -33,7 +42,7 @@ class ApiServiceClazz {
     public createCategory: TCreateCategory<Promise<IList>> = async (data) =>  {
         try {
             const resp = await this.instance.post<IList>(`/list`, data)
-            return resp.data;
+            return await resp.json() as Promise<IList>;
         } catch (e) {
             console.log('createCategory ERROR: ', e)
             throw e;
@@ -44,7 +53,7 @@ class ApiServiceClazz {
     public updateCategory: TUpdateCategory<Promise<IList>> = async (listId, data) => {
         try {
             const resp = await this.instance.patch<IList>(`/list/${listId}`, data)
-            return resp.data
+            return await resp.json() as Promise<IList>
         } catch (e) {
             console.log('updateCategory ERROR: ', e)
             throw e;
@@ -53,8 +62,8 @@ class ApiServiceClazz {
 
     public removeCategory: TRemoveCategory<Promise<{ id: number }>> = async(listId) => {
         try {
-            const resp = await this.instance.delete<{ id: number }>(`/list/${listId}`)
-            return resp.data;
+            await this.instance.delete<{ id: number }>(`/list/${listId}`)
+            return { id: listId }
         } catch (e) {
             console.log('removeCategory ERROR: ', e)
             throw e;
@@ -64,7 +73,7 @@ class ApiServiceClazz {
     public createTodo: TCreateTodo<Promise<ITodo>> = async (listId, data) => {
         try {
             const resp = await this.instance.post<ITodo>(`/list/${listId}/todo`, data)
-            return resp.data
+            return await resp.json() as Promise<ITodo>
         } catch (e) {
             console.log('createTodo ERROR: ', e)
             throw e;
@@ -74,7 +83,7 @@ class ApiServiceClazz {
     public updateTodo: TUpdateTodo<Promise<ITodo>> = async (listId, todoId, data) => {
         try {
             const resp = await this.instance.patch<ITodo>(`/list/${listId}/todo/${todoId}`, data)
-            return resp.data
+            return await resp.json() as Promise<ITodo>
         } catch (e) {
             console.log('updateTodo ERROR: ', e)
             throw e;
@@ -83,8 +92,8 @@ class ApiServiceClazz {
 
     public removeTodo: TRemoveTodo<Promise<{ id: number }>> = async (listId, todoId) => {
         try {
-            const resp = await this.instance.delete<{ id: number }>(`/list/${listId}/todo/${todoId}`)
-            return resp.data;
+            await this.instance.delete<{ id: number }>(`/list/${listId}/todo/${todoId}`)
+            return { id: todoId }
         } catch (e) {
             console.log('removeTodo ERROR: ', e)
             throw e;
